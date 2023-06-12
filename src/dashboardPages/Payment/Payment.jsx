34667762e22f1d45f33f699d44c508484f760679
Md. Abu Nayeem <niloy1882@gmail.com>
2023-06-12 22:36:59 +0600

    2 sections added

--- src/dashboardPages/Payment/Payment.jsx
+++ src/dashboardPages/Payment/Payment.jsx
@@ -1,7 +1,7 @@
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
-import CheckOutForm from './CheckOutForm';
+import CheckOutForm from './CheckoutForm';
 import UseClass from '../../hooks/UseClass';
 const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 const Payment = () => {
